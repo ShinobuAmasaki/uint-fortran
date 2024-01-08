cee5673f6a5ec10e295eb839c2f83899201d63fd
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-08 13:55:50 +0900

    [mod] undefined operators for uint8 are comment-outed.

--- src/uint8_m.f90
+++ src/uint8_m.f90
@@ -3,13 +3,13 @@ module uint8_m
    use :: assignment_m, only: assignment(=)
 
    use :: addition_m,       only: operator(+)
-   use :: subtraction_m,    only: operator(-)
-   use :: multiplication_m, only: operator(*)
-   use :: division_m,       only: operator( / )
+   ! use :: subtraction_m,    only: operator(-)
+   ! use :: multiplication_m, only: operator(*)
+   ! use :: division_m,       only: operator( / )
 
-   use :: greater_than_m, only: operator(>),  operator(>=)
-   use :: less_than_m,    only: operator(<),  operator(<=)
-   use :: equivalence_m,  only: operator(==), operator(/=)
+   ! use :: greater_than_m, only: operator(>),  operator(>=)
+   ! use :: less_than_m,    only: operator(<),  operator(<=)
+   ! use :: equivalence_m,  only: operator(==), operator(/=)
 
    use :: mod_m, only: mod
    use :: int_m, only: int2, int4
