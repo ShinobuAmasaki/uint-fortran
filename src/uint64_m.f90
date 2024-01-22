5f3d8cccfa5c2b330a20939be3a98bd247397ab1
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-22 19:57:59 +0900

    [update] uint64_m.

--- src/uint64_m.f90
+++ src/uint64_m.f90
@@ -2,7 +2,10 @@ module uint64_m
    use :: uint64_t
    use :: assignment_m, only: assignment(=)
    use :: addition_m, only: operator(+)
+   use :: subtraction_m, only: operator(-)
+   ! No other arithmetic operators will be provided. 
 
-   ! No other operators will be provided. 
+   use :: equivalence_m, only: operator(==), operator(/=)
+
 
 end module uint64_m
