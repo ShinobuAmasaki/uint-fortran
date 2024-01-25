6f890d423646ab62a8a0bc93a714fda125ac5c96
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-25 15:17:11 +0900

    [del] old type conversions removed in uint16_t, uint32_t.

--- src/types/uint32_t.f90
+++ src/types/uint32_t.f90
@@ -28,10 +28,7 @@ contains
    
    end function validate
 
-
-   !==================================================================!
    ! Casting
-
    pure integer(int64) function cast_to_int64(ua)
       use, intrinsic :: iso_fortran_env
       implicit none
@@ -40,42 +37,5 @@ contains
       cast_to_int64 = validate(ua)
 
    end function cast_to_int64
-
-
-   pure real(real64) function cast_to_dble(ua)
-      use, intrinsic :: iso_fortran_env
-      implicit none
-      type(uint32), intent(in) :: ua
-
-      cast_to_dble = dble(validate(ua))
-   end function cast_to_dble
-
-
-   pure real(real32) function cast_to_real(ua)
-      use, intrinsic :: iso_fortran_env
-      implicit none
-      type(uint32), intent(in) :: ua
-      cast_to_real = real(validate(ua))
-   end function cast_to_real
-
-
-   pure elemental function  cast_to_complex_64_re(ua, im) result(res)
-      use, intrinsic :: iso_fortran_env
-      implicit none
-      type(uint32), intent(in) :: ua
-      real(real64), intent(in) :: im
-      complex(real64) :: res
-      res = cmplx(validate(ua), im, kind(0d0))
-   end function cast_to_complex_64_re
-
-   pure elemental function  cast_to_complex_64_im(re, ua) result(res)
-      use, intrinsic :: iso_fortran_env
-      implicit none
-      type(uint32), intent(in) :: ua
-      real(real64), intent(in) :: re
-      complex(real64) :: res
-      res = cmplx(re, validate(ua), kind(0d0))
-   end function cast_to_complex_64_im
-
 
 end module uint32_t
