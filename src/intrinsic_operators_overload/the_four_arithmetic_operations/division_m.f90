23f717800eb3c102d7760d8fb383a4684f8a7bf4
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-25 15:40:25 +0900

    [add] operator(/) for uint8.

--- src/intrinsic_operators_overload/the_four_arithmetic_operations/division_m.f90
+++ src/intrinsic_operators_overload/the_four_arithmetic_operations/division_m.f90
@@ -1,5 +1,6 @@
 module division_m
    use, intrinsic :: iso_fortran_env
+   use :: uint8_t
    use :: uint16_t 
    use :: uint32_t
    use :: assignment_m
@@ -7,6 +8,12 @@ module division_m
    
    public :: operator( / )
    interface operator(/)
+      module procedure :: uint8_div_uint8
+      module procedure :: uint8_div_int8, int8_div_uint8
+      module procedure :: uint8_div_int16, int16_div_uint8
+      module procedure :: uint8_div_int32, int32_div_uint8
+      module procedure :: uint8_div_int64, int64_div_uint8
+
       module procedure :: uint16_div_int16, int16_div_uint16
       module procedure :: uint16_div_int32, int32_div_uint16
       module procedure :: uint16_div_int64, int64_div_uint16
@@ -19,6 +26,91 @@ module division_m
 
 
 contains
+!-- uint8 ------------------------------------------------------------!
+
+   pure elemental function uint8_div_uint8 (ua, ub) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua, ub
+      integer(int32) :: res
+
+      res = int(cast_to_int16(ua) / cast_to_int16(ub), kind=int32)
+   end function uint8_div_uint8
+
+   pure elemental function uint8_div_int8 (ua, b) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int8), intent(in) :: b
+      integer(int32) :: res
+
+      res = int(cast_to_int16(ua) / b, kind=int32)
+   end function uint8_div_int8
+
+   pure elemental function int8_div_uint8(a, ub) result(res)
+      implicit none
+      integer(int8), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      integer(int32) :: res
+
+      res = int(a / cast_to_int16(ub), kind=int32)
+   end function int8_div_uint8
+
+   pure elemental function uint8_div_int16 (ua, b) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int16), intent(in) :: b
+      integer(int32) :: res
+
+      res = int(cast_to_int16(ua) / b, kind=int32)
+   end function uint8_div_int16
+
+   pure elemental function int16_div_uint8(a, ub) result(res)
+      implicit none
+      integer(int16), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      integer(int32) :: res
+
+      res = int(a / cast_to_int16(ub), kind=int32)
+   end function int16_div_uint8
+
+
+   pure elemental function uint8_div_int32 (ua, b) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int32), intent(in) :: b
+      integer(int32) :: res
+
+      res = int(cast_to_int16(ua) / b, kind=int32)
+   end function uint8_div_int32
+
+   pure elemental function int32_div_uint8(a, ub) result(res)
+      implicit none
+      integer(int32), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      integer(int32) :: res
+
+      res = int(a / cast_to_int16(ub), kind=int32)
+   end function int32_div_uint8
+
+   pure elemental function uint8_div_int64 (ua, b) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int64), intent(in) :: b
+      integer(int64) :: res
+
+      res = int(cast_to_int16(ua) / b, kind=int64)
+   end function uint8_div_int64
+
+   pure elemental function int64_div_uint8(a, ub) result(res)
+      implicit none
+      integer(int64), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      integer(int64) :: res
+
+      res = int(a / cast_to_int16(ub), kind=int64)
+   end function int64_div_uint8
+
+!-- uint16 -----------------------------------------------------------!
+
    pure elemental function uint16_div_uint16 (ua, ub) result(res)
       use, intrinsic :: iso_fortran_env
       implicit none
@@ -91,6 +183,8 @@ contains
 
       res = cast_to_int32(ua) / a  
    end function
+
+!-- uint32 -----------------------------------------------------------!
 
    pure elemental function uint32_div_uint32(ua, ub) result(res)
       use, intrinsic :: iso_fortran_env
