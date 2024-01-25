b5771976c94f1a4463dba7e3eda13a8d9e40ee2e
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-25 14:46:42 +0900

    [add] uint8 comparison in equivalence_m.

--- src/intrinsic_operators_overload/comparisons/equivalence_m.f90
+++ src/intrinsic_operators_overload/comparisons/equivalence_m.f90
@@ -1,4 +1,5 @@
 module equivalence_m
+   use :: uint8_t
    use :: uint16_t 
    use :: uint32_t 
    use :: uint64_t
@@ -6,6 +7,13 @@ module equivalence_m
 
    public :: operator(==)
    interface operator(==)
+      module procedure :: uint8_eq_uint8
+      module procedure :: uint8_eq_int8, int8_eq_uint8
+      module procedure :: uint8_eq_int16, int16_eq_uint8
+      module procedure :: uint8_eq_int32, int32_eq_uint8
+      module procedure :: uint8_eq_int64, int64_eq_uint8
+      
+
       module procedure :: uint16_eq_uint16
       module procedure :: uint16_eq_int16, int16_eq_uint16
       module procedure :: uint16_eq_int32, int32_eq_uint16
@@ -23,6 +31,12 @@ module equivalence_m
    !---------------------------------------------------------!
    public :: operator(/=)
    interface operator(/=)
+      module procedure :: uint8_ne_uint8
+      module procedure :: uint8_ne_int8, int8_ne_uint8
+      module procedure :: uint8_ne_int16, int16_ne_uint8
+      module procedure :: uint8_ne_int32, int32_ne_uint8
+      module procedure :: uint8_ne_int64, int64_ne_uint8
+
       module procedure :: uint16_ne_uint16
       module procedure :: uint16_ne_int16, int16_ne_uint16
       module procedure :: uint16_ne_int32, int32_ne_uint16
@@ -39,7 +53,99 @@ module equivalence_m
 
 contains
    
-   ! equal to
+!== equal to =========================================================!
+
+!-- uint8 ------------------------------------------------------------!
+   pure elemental function uint8_eq_uint8 (ua, ub) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua, ub
+      logical :: res
+
+      res = ua%u8 == ub%u8
+   end function uint8_eq_uint8
+   
+   pure elemental function uint8_eq_int8 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int8), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) == b
+   end function uint8_eq_int8
+
+   pure elemental function int8_eq_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int8), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a == cast_to_int16(ub)
+   end function int8_eq_uint8
+
+   pure elemental function uint8_eq_int16 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int16), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) == b
+   end function uint8_eq_int16
+
+   pure elemental function int16_eq_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int16), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a == cast_to_int16(ub)
+   end function int16_eq_uint8
+
+   pure elemental function uint8_eq_int32 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int32), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) == b
+   end function uint8_eq_int32
+
+   pure elemental function int32_eq_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int32), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a == cast_to_int16(ub)
+   end function int32_eq_uint8
+
+   pure elemental function uint8_eq_int64 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int64), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) == b
+   end function uint8_eq_int64
+
+   pure elemental function int64_eq_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int64), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a == cast_to_int16(ub)
+   end function int64_eq_uint8
+
+!-- uint16 -----------------------------------------------------------!
+
    pure elemental function uint16_eq_uint16 (ua, ub) result(res)
       implicit none
       type(uint16), intent(in) :: ua, ub
@@ -100,6 +206,8 @@ contains
 
       res = a == cast_to_int32(ub)
    end function int64_eq_uint16
+
+!-- uint32 -----------------------------------------------------------!
 
    pure elemental function uint32_eq_uint32 (ua, ub) result(res)
       use, intrinsic :: iso_fortran_env
@@ -225,7 +333,97 @@ contains
    end function int64_eq_uint64
 
 !== Not equal ========================================================!
+
+!-- uint8 ------------------------------------------------------------!
    
+   pure elemental function uint8_ne_uint8 (ua, ub) result(res)
+      implicit none
+      type(uint8), intent(in) :: ua, ub
+      logical :: res
+
+      res = ua%u8 /= ub%u8
+   end function uint8_ne_uint8
+   
+   pure elemental function uint8_ne_int8 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int8), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) /= b
+   end function uint8_ne_int8
+
+   pure elemental function int8_ne_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int8), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a /= cast_to_int16(ub)
+   end function int8_ne_uint8
+
+   pure elemental function uint8_ne_int16 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int16), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) /= b
+   end function uint8_ne_int16
+
+   pure elemental function int16_ne_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int16), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a /= cast_to_int16(ub)
+   end function int16_ne_uint8
+
+   pure elemental function uint8_ne_int32 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int32), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) /= b
+   end function uint8_ne_int32
+
+   pure elemental function int32_ne_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int32), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a /= cast_to_int16(ub)
+   end function int32_ne_uint8
+
+   pure elemental function uint8_ne_int64 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint8), intent(in) :: ua
+      integer(int64), intent(in) :: b
+      logical :: res
+
+      res = cast_to_int16(ua) /= b
+   end function uint8_ne_int64
+
+   pure elemental function int64_ne_uint8(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int64), intent(in) :: a
+      type(uint8), intent(in) :: ub
+      logical :: res
+
+      res = a /= cast_to_int16(ub)
+   end function int64_ne_uint8
+
    pure elemental function uint16_ne_uint16 (ua, ub) result(res)
       implicit none
       type(uint16), intent(in) :: ua, ub
--- src/uint8_m.f90
+++ src/uint8_m.f90
@@ -9,7 +9,7 @@ module uint8_m
 
    ! use :: greater_than_m, only: operator(>),  operator(>=)
    ! use :: less_than_m,    only: operator(<),  operator(<=)
-   ! use :: equivalence_m,  only: operator(==), operator(/=)
+   use :: equivalence_m,  only: operator(==), operator(/=)
 
    use :: mod_m, only: mod
    use :: int_m, only: int2, int4
