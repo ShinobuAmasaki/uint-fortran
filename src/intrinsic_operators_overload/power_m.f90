efa2562797635757478cca9e32f1eb82611b2cff
Amasaki Shinobu <amasaki203@gmail.com>
2024-01-26 18:47:44 +0900

    [add] power operation for uint16.

--- src/intrinsic_operators_overload/power_m.f90
+++ src/intrinsic_operators_overload/power_m.f90
@@ -13,10 +13,11 @@ module power_m
       module procedure :: uint8_pow_int32, int32_pow_uint8
       module procedure :: uint8_pow_int64, int64_pow_uint8 
 
-      ! module procedure :: uint16_pow_uint16
-      ! module procedure :: uint16_pow_int16, int16_pow_uint16
-      ! module procedure :: uint16_pow_int32, int32_pow_uint16
-      ! module procedure :: uint16_pow_int64, int64_pow_uint16 
+      module procedure :: uint16_pow_uint16
+      module procedure :: uint16_pow_int8, int8_pow_uint16
+      module procedure :: uint16_pow_int16, int16_pow_uint16
+      module procedure :: uint16_pow_int32, int32_pow_uint16
+      module procedure :: uint16_pow_int64, int64_pow_uint16 
 
       module procedure :: uint32_pow_uint32
       module procedure :: uint32_pow_int32, int32_pow_uint32
@@ -24,6 +25,8 @@ module power_m
    end interface
 
 contains
+
+!-- uint8 ------------------------------------------------------------!
 
    pure elemental function uint8_pow_uint8(ua, ub) result(res)
       use, intrinsic :: iso_fortran_env
@@ -114,6 +117,95 @@ contains
       res = a ** cast_to_int16(ub)
    end function int64_pow_uint8
 
+!-- uint16 -----------------------------------------------------------!
+   pure elemental function uint16_pow_uint16(ua, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint16), intent(in) :: ua, ub
+      integer(int64) :: res
+
+      res = cast_to_int32(ua)**cast_to_int32(ub)
+   end function  uint16_pow_uint16
+
+   pure elemental function uint16_pow_int8 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint16), intent(in) :: ua
+      integer(int8), intent(in) :: b
+      integer(int64) :: res
+
+      res = cast_to_int32(ua) ** int(b, kind=int16)
+   end function uint16_pow_int8
+
+   pure elemental function int8_pow_uint16(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int8), intent(in) :: a
+      type(uint16), intent(in) :: ub
+      integer(int64) :: res
+
+      res = a ** cast_to_int32(ub)
+   end function int8_pow_uint16
+
+   pure elemental function uint16_pow_int16 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint16), intent(in) :: ua
+      integer(int16), intent(in) :: b
+      integer(int64) :: res
+
+      res = cast_to_int32(ua) ** b
+   end function uint16_pow_int16
+
+   pure elemental function int16_pow_uint16(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int16), intent(in) :: a
+      type(uint16), intent(in) :: ub
+      integer(int64) :: res
+
+      res = a ** cast_to_int32(ub)
+   end function int16_pow_uint16
+
+   pure elemental function uint16_pow_int32 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint16), intent(in) :: ua
+      integer(int32), intent(in) :: b
+      integer(int64) :: res
+
+      res = cast_to_int32(ua) ** b
+   end function uint16_pow_int32
+
+   pure elemental function int32_pow_uint16(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int32), intent(in) :: a
+      type(uint16), intent(in) :: ub
+      integer(int64) :: res
+
+      res = a ** cast_to_int32(ub)
+   end function int32_pow_uint16
+
+   pure elemental function uint16_pow_int64 (ua, b) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      type(uint16), intent(in) :: ua
+      integer(int64), intent(in) :: b
+      integer(int64) :: res
+
+      res = cast_to_int32(ua) ** b
+   end function uint16_pow_int64
+
+   pure elemental function int64_pow_uint16(a, ub) result(res)
+      use, intrinsic :: iso_fortran_env
+      implicit none
+      integer(int64), intent(in) :: a
+      type(uint16), intent(in) :: ub
+      integer(int64) :: res
+
+      res = a ** cast_to_int32(ub)
+   end function int64_pow_uint16
 !-- uint32 -----------------------------------------------------------!
 
    pure elemental function uint32_pow_uint32(ua, ub) result(res)
